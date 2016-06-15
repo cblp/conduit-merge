8a79afd3c71cbaf5556506c9bc00d0293cb1fcfd
Yuriy Syrovetskiy <cblp@cblp.su>
2016-06-15 19:11:26 +0300

    [+] it works!

---
+++ src/Data/Conduit/Merge.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE RankNTypes #-}
+
+module Data.Conduit.Merge (mergeSources) where
+
+import Control.Monad.Trans (lift)
+import Data.Conduit        (Producer, Source, await, newResumableSource, yield, ($$++))
+import Data.List           (sortOn)
+
+mergeSources :: (Ord a, Monad m) => [Source m a] -> Producer m a
+mergeSources = mergeResumable . map newResumableSource
+  where
+    mergeResumable sources = do
+        prefetchedSources <- lift $ mapM ($$++ await) sources
+        go [(a, s) | (s, Just a) <- prefetchedSources]
+    go [] = pure ()
+    go sources = do
+        let (a, src1) : sources1 = sortOn fst sources
+        yield a
+        (src2, mb) <- lift $ src1 $$++ await
+        let sources2 = case mb of
+                Nothing -> sources1
+                Just b  -> (b, src2) : sources1
+        go sources2
