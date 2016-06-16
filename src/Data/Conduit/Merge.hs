75caa97aa91ff0f4282c6e8ad0992e68d60c8db5
Yuriy Syrovetskiy <cblp@cblp.su>
2016-06-16 11:53:20 +0300

    v 0.1.1.0 [+] mergeSourcesOn

--- src/Data/Conduit/Merge.hs
+++ src/Data/Conduit/Merge.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE RankNTypes #-}
 
-module Data.Conduit.Merge (mergeSources) where
+module Data.Conduit.Merge (mergeSources, mergeSourcesOn) where
 
 import Control.Monad.Trans (lift)
 import Data.Conduit        (Producer, Source, await, newResumableSource, yield, ($$++))
@@ -8,14 +8,18 @@ import Data.List           (sortOn)
 
 -- | Merge multiple sorted source into one sorted producer.
 mergeSources :: (Ord a, Monad m) => [Source m a] -> Producer m a
-mergeSources = mergeResumable . map newResumableSource
+mergeSources = mergeSourcesOn id
+
+-- | Merge multiple sorted source into one sorted producer using specified sorting key.
+mergeSourcesOn :: (Ord b, Monad m) => (a -> b) -> [Source m a] -> Producer m a
+mergeSourcesOn key = mergeResumable . fmap newResumableSource
   where
     mergeResumable sources = do
-        prefetchedSources <- lift $ mapM ($$++ await) sources
+        prefetchedSources <- lift $ traverse ($$++ await) sources
         go [(a, s) | (s, Just a) <- prefetchedSources]
     go [] = pure ()
     go sources = do
-        let (a, src1) : sources1 = sortOn fst sources
+        let (a, src1) : sources1 = sortOn (key . fst) sources
         yield a
         (src2, mb) <- lift $ src1 $$++ await
         let sources2 = case mb of
