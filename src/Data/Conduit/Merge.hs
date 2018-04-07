8fd4a059745205d1231a4f945896f338c2c978ca
Galen Huntington <galenhuntington@gmail.com>
2018-04-07 04:50:12 -0700

    Conduit has replaced Resumable with Sealed. (#1)

--- src/Data/Conduit/Merge.hs
+++ src/Data/Conduit/Merge.hs
@@ -3,20 +3,20 @@
 module Data.Conduit.Merge (mergeSources, mergeSourcesOn) where
 
 import Control.Monad.Trans (lift)
-import Data.Conduit        (Producer, Source, await, newResumableSource, yield, ($$++))
+import Data.Conduit        (ConduitT, await, sealConduitT, yield, ($$++))
 import Data.Foldable       (toList)
 import Data.List           (sortOn)
 
 -- | Merge multiple sorted sources into one sorted producer.
-mergeSources :: (Ord a, Foldable f, Monad m) => f (Source m a) -> Producer m a
+mergeSources :: (Ord a, Foldable f, Monad m) => f (ConduitT () a m ()) -> ConduitT i a m ()
 mergeSources = mergeSourcesOn id
 {-# INLINE mergeSources #-}
 
 -- | Merge multiple sorted sources into one sorted producer using specified sorting key.
-mergeSourcesOn :: (Ord b, Foldable f, Monad m) => (a -> b) -> f (Source m a) -> Producer m a
-mergeSourcesOn key = mergeResumable . fmap newResumableSource . toList
+mergeSourcesOn :: (Ord b, Foldable f, Monad m) => (a -> b) -> f (ConduitT () a m ()) -> ConduitT i a m ()
+mergeSourcesOn key = mergeSealed . fmap sealConduitT . toList
   where
-    mergeResumable sources = do
+    mergeSealed sources = do
         prefetchedSources <- lift $ traverse ($$++ await) sources
         go [(a, s) | (s, Just a) <- prefetchedSources]
     go [] = pure ()
