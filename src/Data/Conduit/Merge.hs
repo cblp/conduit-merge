52adf417e4643c3c82aefa521871dd75c3dfe7df
Yuriy Syrovetskiy <cblp@cblp.su>
2019-05-27 10:45:41 +0300

    Fix -Wincomplete-patterns

--- src/Data/Conduit/Merge.hs
+++ src/Data/Conduit/Merge.hs
@@ -13,18 +13,20 @@ mergeSources = mergeSourcesOn id
 {-# INLINE mergeSources #-}
 
 -- | Merge multiple sorted sources into one sorted producer using specified sorting key.
-mergeSourcesOn :: (Ord b, Foldable f, Monad m) => (a -> b) -> f (ConduitT () a m ()) -> ConduitT i a m ()
-mergeSourcesOn key = mergeSealed . fmap sealConduitT . toList
-  where
+mergeSourcesOn
+    :: (Ord b, Foldable f, Monad m)
+    => (a -> b) -> f (ConduitT () a m ()) -> ConduitT i a m ()
+mergeSourcesOn key = mergeSealed . fmap sealConduitT . toList where
     mergeSealed sources = do
         prefetchedSources <- lift $ traverse ($$++ await) sources
         go [(a, s) | (s, Just a) <- prefetchedSources]
-    go [] = pure ()
-    go sources = do
-        let (a, src1) : sources1 = sortOn (key . fst) sources
-        yield a
-        (src2, mb) <- lift $ src1 $$++ await
-        let sources2 = case mb of
-                Nothing -> sources1
-                Just b  -> (b, src2) : sources1
-        go sources2
+    go sources =
+        case sortOn (key . fst) sources of
+            []                   -> pure ()
+            (a, src1) : sources1 -> do
+                yield a
+                (src2, mb) <- lift $ src1 $$++ await
+                let sources2 = case mb of
+                        Nothing -> sources1
+                        Just b  -> (b, src2) : sources1
+                go sources2
